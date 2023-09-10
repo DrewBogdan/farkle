2746bf74ec71a3b33ef69df1e7c0597e7a0213b9
Aaron S. Crandall <crandall@gonzaga.edu>
2023-09-10 12:03:38 -0700

    Fully working example!

--- crandallFarkle.cpp
+++ crandallFarkle.cpp
@@ -1,9 +1,12 @@
 /*
- *  Crandall's Purpousfully Mediocre Farkle Rolling in C++
+ *  Crandall's Purposefully Mediocre Farkle Rolling in C++
  *
  *  Contributors:
  *      Aaron S. Crandall <crandall@gonzaga.edu>
+ * 
  *  Copyright: 2023
+ * 
+ *  License: Only for class uses
  *
  */
 
@@ -17,16 +20,22 @@ using namespace std;
 
 
 
-
 int main(int argc, char* argv[]) {
     srand(time(0));
-    cout << "Farkle Rolling and Scoring" << endl;
+    cout << "Farkle Rolling and Scoring Terrible C++ Example" << endl;
 
     int dice[6];
     for( int i = 0; i < 6; i++ ) {
         dice[i] = rand() % 6 + 1;
     }
 
+    // Testing hand with known values
+    // int testHand[6] = { 1, 5, 5, 3, 4, 5};
+    // for( int i = 0; i < 6; i++ ) {
+    //    dice[i] = testHand[i];
+    // }
+
+    // Sort the hand the hard way
     for (int i = 0; i < 6 - 1; i++) {
         for (int j = 0; j < 6 - i - 1; j++) {
             if (dice[j] > dice[j + 1]) {
@@ -36,13 +45,6 @@ int main(int argc, char* argv[]) {
             }
         }
     }
-
-    // Testing hand with known values
-    int testHand[6] = { 2, 2, 3, 6, 6, 6};
-    for( int i = 0; i < 6; i++ ) {
-        dice[i] = testHand[i];
-    }
-
 
     cout << "Hand: ";
     for( int i = 0; i < 6; i++ ) {
@@ -99,6 +101,7 @@ int main(int argc, char* argv[]) {
 
         while( !done) {
             // Print status
+            cout << endl;
 cout << "*************************** Current hand and meld *******************" << endl;
 cout << " Die   Hand |   Meld" << endl;
 cout << "------------+---------------" << endl;
@@ -120,19 +123,83 @@ for(int i = 0; i < 6; i++ ) {
 }
 
 cout << "------------+---------------" << endl;
-cout << "                Meld is: ";
 bool isValidMeld = false;
 
-// TODO: Count valid meld
-// Calculate meld score too
+    {   // Calculate the meld
+        meldScore = 0; // Reset each time user changes meld dice
+        int meldDiceCount = 0;
+        int meldDice[6] = {0,0,0,0,0,0};
+        for(int i = 0; i < 6; i++ ) {
+            if( meld[i] != 0 ) {
+                meldDice[meldDiceCount] = meld[i];
+                meldDiceCount++;
+            }
+        }
+
+        int meldDiceSizesCount[7] = {0,0,0,0,0,0,0};
+        for( int i = 0; i < 6; i++ ) {
+            meldDiceSizesCount[meldDice[i]]++;
+        }
+
+        // cout << endl;
+        // cout << "Meld dice sizes count: ";
+        // for( int i = 1; i < 7; i++ ) {
+        //    cout << meldDiceSizesCount[i] << " ";
+        // }
+        // cout << endl;
+
+        // Check straight
+        bool isStraight = true;
+        for( int i = 1; i < 7; i++ ) {
+            if( meldDiceSizesCount[i] != 1 ) {
+                isStraight = false;
+            }
+        }
+        if( isStraight ) {
+            meldScore += 1000;
+        } else {
+            // Check 3 pairs
+            int pairCount = 0;
+            for( int i = 1; i < 7; i++ ) {
+                if( meldDiceSizesCount[i] == 2) {
+                    pairCount++;
+                }
+            }
+            if( pairCount == 3 ) {
+                meldScore += 750;
+            } else {
+                // Check triples +
+                bool isTripleSet = false;
+                for( int i = 1; i < 7; i++ ) {
+                    if( meldDiceSizesCount[i] >= 3 ) {
+                        // Found triple (or more!)
+                        isTripleSet = true;
+                        int tripleSetPoints = 0;
+                        if(i == 1) {
+                            tripleSetPoints = 1000;
+                        } else {
+                            tripleSetPoints = i * 100;
+                        }
+                        if( meldDiceSizesCount[i] > 3 ) { // 4 + in set
+                            tripleSetPoints += (meldDiceSizesCount[i] - 3) * 100 * i;
+                        }
+                        meldScore += tripleSetPoints;
+                    }
+                }
+
+                // Add 1's & 5's if unused
+                if( meldDiceSizesCount[1] < 3) {
+                    meldScore += meldDiceSizesCount[1] * 100;
+                }
+
+                if( meldDiceSizesCount[5] < 3) {
+                    meldScore += meldDiceSizesCount[5] * 50;
+                }
+            }
+        }
+    }
 
 
-if(isValidMeld) {
-    cout << "Valid";
-} else {
-    cout << "Invalid";
-}
-cout << endl;
 
 cout << "                Meld Score: " << meldScore << endl;
 cout << endl;
